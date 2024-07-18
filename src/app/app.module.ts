600d3abba186fefbd60a8621aa86f94395bfe509
lnathanprove <lnathan@prove.com>
2024-07-18 11:26:40 -0400

    CUSTPORTAL-838 Review page additions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { appRoutingModule } from "./app.routes";
-import { ChallengePageComponent } from "./challenge-page/challenge-page.component";
-import { SMSWaitingComponent } from "./sms-waiting-page/sms-waiting-page.component";
+import { AppComponent } from './app.component';
+import { appRoutingModule } from './app.routes';
+import { ChallengePageComponent } from './challenge-page/challenge-page.component';
+import { SMSWaitingComponent } from './sms-waiting-page/sms-waiting-page.component';
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ChallengePageComponent,
-        SMSWaitingComponent
-    ],
-    imports: [
-        BrowserModule,
-        appRoutingModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent, ChallengePageComponent, SMSWaitingComponent],
+  imports: [
+    BrowserModule,
+    appRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+  ],
+  //providers: [CommonModule, provideAnimations()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
