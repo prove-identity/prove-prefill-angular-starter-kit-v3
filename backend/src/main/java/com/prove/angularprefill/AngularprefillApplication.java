ff2bf2841d9f7055288d2f23cd57d94d2d092186
lnathanprove <lnathan@prove.com>
2024-08-15 11:56:27 -0400

    Bug fixing attempts

---
+++ backend/src/main/java/com/prove/angularprefill/AngularprefillApplication.java
@@ -0,0 +1,13 @@
+package com.prove.angularprefill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularprefillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularprefillApplication.class, args);
+	}
+
+}
