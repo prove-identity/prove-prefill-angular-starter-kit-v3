684491c1b2929182a311dc99103562f87defe8ae
lnathanprove <lnathan@prove.com>
2024-08-22 16:42:41 -0400

    Backend updates

---
+++ backend/angular-prefill-starter-kit-v3/src/main/java/com/prove/angularstarterkit/angular_prefill_starter_kit_v3/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.prove.angularstarterkit.angular_prefill_starter_kit_v3.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException(String  message) {
+    super(message);
+  }
+}
--- backend/angular-prefill-starter-kit-v3/src/main/java/com/prove/angularstarterkit/angular_prefill_starter_kit_v3/repo/UserRepo.java
+++ backend/angular-prefill-starter-kit-v3/src/main/java/com/prove/angularstarterkit/angular_prefill_starter_kit_v3/repo/UserRepo.java
@@ -3,5 +3,8 @@ package com.prove.angularstarterkit.angular_prefill_starter_kit_v3.repo;
 import com.prove.angularstarterkit.angular_prefill_starter_kit_v3.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
+  Optional<User> findUserById(Long id);
 }
